ab4b74b41404a0b6ba0e0307b9f00e565f369aff
Justin M. Ramon <justinmramon@gmail.com>
2022-05-18 23:12:49 -0500

    commit tree 22d1929edb09c61bc1a0a4c3d71ed8f493150bd9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(soul){
+    return function fareMultiplier(glo){
+        return soul * glo
+    }
+}
+
+const fareDoubler = function(createFareMultiplier){
+    return createFareMultiplier*2
+}
+
+const fareTripler = function(createFareMultiplier){
+    return createFareMultiplier*3
+}
+
+function selectDifferentDrivers(drivers, functions){
+    return functions(drivers)
+}
